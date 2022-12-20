53c3c0e232084c3915255b074cf5bd7f43c6b981
Sam Johnson <samj@hey.com>
2022-12-20 15:29:11 +0900

    Add content fetching from database

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
 import React from "react";
 
-const CoursesHomePage = () => {
-  const courses = [
-    {
-      id: 1,
-      name: "Japanese Fundamentals",
-      path: "japanese_fundamentals",
-    },
-    {
-      id: 2,
-      name: "JLPT: N5",
-      path: "jlpt_n5",
-    },
-  ];
+import prisma from '../../lib/prisma';
+
+export const getStaticProps = async () => {
+  const courses = await prisma.course.findMany({
+    select: {
+      title: true,
+      description: true,
+      id: true,
+    }
+  });
+  return {
+    props: { courses },
+  };
+};
+
+const CoursesHomePage = ({ courses }) => {
   return (
     <div>
       <h1>Courses Home Page</h1>
@@ -21,13 +24,16 @@ const CoursesHomePage = () => {
         {courses.map((course) => (
           <li>
             <Link
+              className="text-5xl"
               href={{
-                pathname: "courses/[path]",
-                query: { path: course.path },
+                pathname: `courses/${course.id}`,
+                query: { id: course.id },
               }}
             >
-              {course.name}
+              {course.title}
             </Link>
+            <br />
+            <span className="text-2xl text-green-500">{course.description}</span>
           </li>
         ))}
       </ul>
